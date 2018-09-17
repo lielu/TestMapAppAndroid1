7861d29b6b6528cc2399cae5d281d39dcd9c88dc
lielu <lielu@users.noreply.github.com>
2018-09-17 00:49:28 -0700

    Test App with Google Maps
    Test App with Google Maps

---
+++ feature/src/main/java/com/lielu/testmapappandroid1/feature/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.lielu.testmapappandroid1.feature
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                //message.setText(R.string.title_home)
+                startActivity(Intent(this, BasicMapDemoActivity::class.java))
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                //message.setText(R.string.title_dashboard)
+                startActivity(Intent(this, MarkerDemoActivity::class.java))
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
